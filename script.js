82be0db059bef27d5c6844a4d9c23f74ecb33cc2
Madsag89 <71001617+Madsag89@users.noreply.github.com>
2021-05-21 13:12:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//Burger bar toggle
+
+document.addEventListener('DOMContentLoaded', nav)
+function nav(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');  
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+    })
+};
+
+
+
+
+
+//Chat funktion
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+
+
+
+//Shrink header
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("yuniklogo").style.maxHeight = "90px";
+    document.getElementById("yuniklogo").style.marginTop = "5px";
+    document.getElementById("burger").style.marginTop = "-10px";
+    document.getElementById("header").style.height = "10%";
+
+  } else {
+    document.getElementById("yuniklogo").style.maxHeight = "180px";
+    document.getElementById("yuniklogo").style.marginTop = "60px";
+    document.getElementById("burger").style.marginTop = "50px";
+    document.getElementById("header").style.height = "0%";
+
+  }
+}
+
+
---
+++ yunik-typed.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.anim', {
+    strings: 
+    ['You',
+     'Yunik'],
+    
+     backSpeed: 150,
+     typeSpeed: 150,
+     loop: false,
+     smartBackspace: true,
+     startDelay: 500,
+     backDelay: 700,
+    });
+    
